558b689164161cea1a91620762d5695f7aaf1c6b
Brian Yang <yangbri1@msu.edu>
2025-01-17 19:21:04 +0000

    Used a while loop to find the 'str' in reverse, implement String .equals() method to compare values of 'str' to 'reverseStr', tied it up using a ternary operator showing whether or not the given 'str' fulfills the definition of a palindrome.

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,30 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        // initialize an empty String to append char of 'str' in reverse order
+        String reverseString = "";
+
+        // assign iterator 'i' to this value in order to decrement
+        int i = str.length() - 1;
+        // loop through str in descending fashion
+        while(i >= 0){
+            // reverseString += char[i];    -- works for char[]
+            // append each char from right to left
+            reverseString += str.charAt(i);
+            // (prefix) decrement to allow loop to continue
+            --i;
+        }
+        
+        // apply .equals() to loosely compare the value of 'str' to value of 'reverseString' 
+        // use .equals() over equality operator (==) when dealing with String (complex Object types -- memory location possibly different in Heap)
+        // if(str.equals(reverseString)){
+        //     // return true if equal
+        //     return true;
+        // }
+        // // otherwise return false
+        // return false;
+
+        // aside: ternary operator way equivalent above if-else 
+        return(str.equals(reverseString) ? true : false);
     }
 }
